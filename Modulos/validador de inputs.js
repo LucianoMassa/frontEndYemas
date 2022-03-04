d203b6dbcc692f572c4bb8358a28aee551f0b041
lucianomassa <setigex.sj@hotmail.com>
2022-03-03 21:00:00 -0300

    ahora si

--- Modulos/validador de inputs.js
+++
@@ -1,4 +0,0 @@
-
-
-export  const tipnumers = (currentValue) => currentValue > 0;
-
--- Ventas/funciones.js
+++
@@ -1,9 +0,0 @@
-function buscarproducto(){   
-
-    const input = document.getElementById("inputproducto");
-const value = input.value;
-
-const resultado = document.getElementById("resultado");
-resultado.innerText = value;
-}
-
